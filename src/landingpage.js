a65ac441a94f28f3c044e39a0bf2ccf922b16e60
Ayobami <ayobamiwealth@gmail.com>
2023-06-01 23:59:01 +0100

    initial commit

---
+++ src/landingpage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import TextField from "@mui/material/TextField";
+
+const searchdrugs =() => {
+   
+    return(
+       <div className='drugsearchbar'>
+           <h1>Search</h1>
+           <div className='searchbar'>
+            <TextField
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+            />
+           </div>
+       </div>  
+    );
+}
+
+export default searchdrugs;
