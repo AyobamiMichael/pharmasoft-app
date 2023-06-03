6848a8db1489142775d281155f40cf0218088d46
Ayobami <ayobamiwealth@gmail.com>
2023-06-03 12:18:16 +0100

    initial commit

--- src/landingpage/landingpage.js
+++ src/landingpage/landingpage.js
@@ -4,19 +4,12 @@ import TextField from "@mui/material/TextField";
 const Searchdrugs =() => {
    
     return(
-       <div className='drugsearchbar'>
-           <h1>Search</h1>
-           <div className='searchbar'>
-            <TextField
-                id="outlined-basic"
-                variant="outlined"
-                label="Search"
-                
-            />
-           </div>
-          
+       <div className='searchbar'>
+        <input type='search' 
+        placeholder='Search here'
         
-       </div>  
+        />
+       </div>
     );
 }
 
